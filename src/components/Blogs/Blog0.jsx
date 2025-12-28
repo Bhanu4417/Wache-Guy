56e0be6ee3453054e0a94efa27bd83dd86ae5549
Bhanu4417 <nagarbhanu0529@gmail.com>
2025-12-28 22:53:23 +0530

    update

--- src/components/Blogs/Blog0.jsx
+++ src/components/Blogs/Blog0.jsx
@@ -7,7 +7,7 @@ const Blog0 = () => {
   <>
   <div className="flex flex-col min-h-[calc(100dvh-80px)]
  max-w-[720px] mx-auto px-4 sm:px-6 md:px-8 pt-20 pb-24">
-  <div className="text-[#949494] md:mt-1 mt-2 text-[14px] md:text-[16px]">Published on January 1, 2025</div>
+  <div className="text-[#949494] md:mt-1 mt-2 text-[14px] md:text-[16px]">Published on December 28, 2025</div>
 <div className="text-white text-[34px] font-bold md:text-[48px] md:mt-3 mt-2">Starting Things Off !</div>
 <div className="flex gap-2">
  <div className="flex items-center md:mt-2 mt-2">
@@ -36,16 +36,16 @@ const Blog0 = () => {
   : 0
   </div></div></div>
 <img className='md:mt-6 mt-3' src="/Blogs/THE QUEST.svg" />
-<div className="text-white text-[20px] md:mt-3 mt-2">This is the day 0 of my challenge.</div>
-<div className="text-[white] md:text-[18px] text-[15px] mt-3">So today I didn't do much it was the starting of my 2nd sem after a 1 month long holiday I came from college about 5pm.Then had a cup of chai and then just got up and started revising c++ did it for about 1:30 hour and then started off working on new projcet just made the navbar today as I was tired and then I justgo to sleep at 1:00 AM.</div>
+<div className="text-white text-[20px] md:mt-3 mt-2">This is the day 1 of my challenge.</div>
+<div className="text-[white] md:text-[18px] text-[15px] mt-3"><p>So today I didn't do much I started working on my next project the music chat app which allows you to chat,play and see analysis about how much time u listened to the music today I just made the login page and connected the backend to spotify API, I didn't do C++ at all.</p></div>
 <div className="md:mt-8 mt-6">
     <Progress
   title="Slept for"
   icon="🌙"
   subtitle="Sleep score"
   startTime="1:00 AM"
-  endTime="8:00 AM"
-  value={6.0}
+  endTime="9:00 AM"
+  value={8.5}
   max={8}
   color="bg-purple-500"
 />
@@ -55,9 +55,9 @@ const Blog0 = () => {
   title="DSA Practice"
   icon="🧠"
   subtitle="DSA Time (C++)"
-  startTime="6:30 PM"
-  endTime="8:00 PM"
-  value={1.5}
+  startTime="00:00 PM"
+  endTime="00:00 PM"
+  value={0.0}
   max={3}
   color="bg-blue-500"
 />
@@ -68,9 +68,9 @@ const Blog0 = () => {
 title="Project Work"
   icon="🖥️"
   subtitle="Project Time"
-  startTime="9:00 PM"
-  endTime="11:30 PM"
-  value={2.5}
+  startTime="11:00 AM"
+  endTime="1:00 PM"
+  value={7.0}
   max={5}
   color="bg-green-500"
 />
@@ -80,8 +80,6 @@ title="Project Work"
 title="Moeny Earned"
   icon="💸"
   subtitle="Time For It"
-  startTime="0:00 PM"
-  endTime="0:00 PM"
   value={0}
   max={5}
   color="bg-green-500"
@@ -98,9 +96,7 @@ title="Moeny Earned"
     />
   }
   subtitle="Time For It"
-  startTime="0:00 PM"
-  endTime="0:00 PM"
-  value={0}
+  value={4.5}
   max={5}
 />
 
